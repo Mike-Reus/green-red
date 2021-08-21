fef14a4581e71a3dc9dc0e4eeb0bbc0af95dd4ea
Mike <5188083@gmail.com>
2021-08-21 12:39:32 +0200

    Initial Commit

---
+++ green&red/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  green&red
+//
+//  Created by M B on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
